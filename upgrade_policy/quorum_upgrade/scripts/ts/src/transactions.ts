d3b2585bd1372841506df2a132d548611ae5fe38
Tony Lee <chunglee2018@u.northwestern.edu>
2024-03-14 18:15:15 -0400

    ptb

--- upgrade_policy/quorum_upgrade/scripts/ts/src/transactions.ts
+++ upgrade_policy/quorum_upgrade/scripts/ts/src/transactions.ts
@@ -1,27 +1,34 @@
 
-import {TransactionBlock} from "@mysten/sui.js/transactions";
 import { getUpgradeDigest, getActiveAddress, prepareAddressVecSet, signAndExecute } from "./utils";
+import { TransactionArgument, TransactionBlock } from '@mysten/sui.js/transactions';
 
 // =================================================================
 // Constants to update when running the different transactions
 // =================================================================
 
 // Voters. Add all addresses that will be part of the quorum policy.
-const VOTER_1 = '';
-const VOTER_2 = '';
-const VOTER_3 = '';
+const VOTER_1 = '0x754a43970ec22d78110069ca1b45fb20818348363648b6b486edf0045dea559e';
+const VOTER_2 = '0xe55684e6a538d29df4b9f510a6bd3ec17d3dc47fb524839954bf7fcf835b5d0d';
+const VOTER_3 = '0xe0b97bff42fcef320b5f148db69033b9f689555348b2e90f1da72b0644fa37d0';
 // The package id of the `quorum_upgrade_policy` package (published on mainnet)
-const QUORUM_UPGRADE_PACKAGE_ID = `0xae627358027f3b53865d2403ecf5573c91d543a387d653764b650b8f85a2235c`;
+const QUORUM_UPGRADE_PACKAGE_ID = `0x03e8badab460af8b3d4c96b283c4c3994b370c9dd2c1a4e5f80d649c25d305da`;
+const UPGRADE_METADATA: Map<string, Uint8Array> = new Map();
+// Example metadata to be attached
+const text = 'Hello, World!';
+const encoder = new TextEncoder();
+const uint8FromString = encoder.encode(text);
+UPGRADE_METADATA.set('key1', uint8FromString);
+UPGRADE_METADATA.set('key2', new Uint8Array([10, 11, 12]));
 // The upgrade cap of the quorum upgrade policy (resulting from `quorum_upgrade_policy::new`)
-const QUORUM_UPGRADE_CAP_ID = ``;
+const QUORUM_UPGRADE_CAP_ID = `0x8d45558b0cdd2a0590d4c148817998aeb6b8f46fd9865436a1d20c30595c7a8f`;
 // path to the package to publish or upgrade
-const PATH_TO_PACKAGE = '';
+const PATH_TO_PACKAGE = '/Users/tonylee/Documents/apps/upgrade_policy/quorum_upgrade';
 // The package id of the testing package to be upgraded. That is the package id of the package
 // defined at the path above
 const TEST_PACKAGE_ID = ``;
 // The upgrade cap of the package to protect with the quorum upgrade policy
 // That is the upgrade cap of the package defined at the path above
-const TEST_PACKAGE_UPGRADE_CAP_ID = ``;
+const TEST_PACKAGE_UPGRADE_CAP_ID = `0xf643c9af74a77e4435d5ed3e728756f422038844143f96c665fb9e2d4b3dfa60`;
 // Voting cap used for voting.
 // That is the voting cap of the transaction signer
 const VOTING_CAP_ID = ``;
@@ -66,6 +73,57 @@ const proposeUpgrade = (txb: TransactionBlock, quorumUpgradeCapId: string, packa
     });
 
 }
+
+const proposeUpgradeV2 = (txb: TransactionBlock, quorumUpgradeCapId: string, packagePath: string, metadata: Map<string, Uint8Array>) => {
+    const { digest }  = getUpgradeDigest(packagePath);
+
+    const proposedUpgrade = txb.moveCall({
+        target: `${QUORUM_UPGRADE_PACKAGE_ID}::quorum_upgrade_policy::propose_upgrade_v2`,
+        arguments: [
+            txb.object(quorumUpgradeCapId),
+            txb.pure(digest, 'vector<u8>')
+        ]
+    });
+
+    txb.moveCall({
+        target: `${QUORUM_UPGRADE_PACKAGE_ID}::quorum_upgrade_policy::add_all_metadata`,
+        arguments: [
+            txb.object(proposedUpgrade),
+            prepareMetadataVecMap(txb, metadata)
+        ]
+    });
+
+    console.log("Proposed Upgrade Object:", proposedUpgrade);
+
+    txb.moveCall({
+        target: `${QUORUM_UPGRADE_PACKAGE_ID}::quorum_upgrade_policy::share_upgrade_object`,
+        arguments: [
+            txb.object(proposedUpgrade),
+        ]
+    });
+}
+
+const prepareMetadataVecMap = (txb: TransactionBlock, metadata: Map<string, Uint8Array>): TransactionArgument => {
+    const vecMap = txb.moveCall({
+        target: `0x2::vec_map::empty<string::String, vector<u8>>`,
+        typeArguments: ['0x2::sui::vec_map']
+    });
+
+    metadata.forEach((value, key) => {
+        txb.moveCall({
+            target: `0x2::vec_map::insert`,
+            arguments: [
+                vecMap,
+                txb.pure.string(key),
+                txb.pure(value, 'vector<u8>'),
+            ],
+            typeArguments: ['0x1::string::String', 'vector<u8>']
+        });
+    });
+    return vecMap;
+}
+
+
 
 /// Vote for a particular `ProposedUpgrade` shared object.
 /// Use the `ProposedUpgrade` object id defined by the transaction above and the
@@ -117,20 +175,20 @@ const executeTransaction = async () => {
 
     const txb = new TransactionBlock();
 
-    // 1- define a 2 out of 3 quorum upgrade policy
-    newQuorumUpgradeCap(txb, 2, [VOTER_1, VOTER_2, VOTER_3], TEST_PACKAGE_UPGRADE_CAP_ID, getActiveAddress());
+    // // 1- define a 2 out of 3 quorum upgrade policy
+    // newQuorumUpgradeCap(txb, 2, [VOTER_1, VOTER_2, VOTER_3], TEST_PACKAGE_UPGRADE_CAP_ID, getActiveAddress());
 
     // 2- propose an upgrade. Digest is determined automatically via the package path
-    proposeUpgrade(txb, QUORUM_UPGRADE_CAP_ID, PATH_TO_PACKAGE);
+    proposeUpgradeV2(txb, QUORUM_UPGRADE_CAP_ID, PATH_TO_PACKAGE, UPGRADE_METADATA);
 
-    // 3- vote for an upgrade
-    vote(txb, PROPOSED_UPGRADE_ID, VOTING_CAP_ID);
+    // // 3- vote for an upgrade
+    // vote(txb, PROPOSED_UPGRADE_ID, VOTING_CAP_ID);
 
-    // 4- authorize/commit the upgrade
-    authorizeUpgrade(txb, TEST_PACKAGE_ID, PROPOSED_UPGRADE_ID, QUORUM_UPGRADE_CAP_ID, PATH_TO_PACKAGE);
+    // // 4- authorize/commit the upgrade
+    // authorizeUpgrade(txb, TEST_PACKAGE_ID, PROPOSED_UPGRADE_ID, QUORUM_UPGRADE_CAP_ID, PATH_TO_PACKAGE);
 
     // Run against mainnet
-    const res = await signAndExecute(txb, 'mainnet');
+    const res = await signAndExecute(txb, 'testnet');
 
     console.dir(res, { depth: null });
 }
