3d5fa45cad29ddbbb5f30f184249a09f20ed1de4
Damir Shamanaev <damirka.ru@gmail.com>
2024-03-25 21:50:30 +0300

    remove unpublished

--- upgrade_policy/quorum_upgrade/app/src/sources/hooks/useMultisigAddressContext.tsx
+++
@@ -1,9 +0,0 @@
-// Copyright (c) Mysten Labs, Inc.
-// SPDX-License-Identifier: Apache-2.0
-import { useContext } from 'react';
-
-import { MultisigAddressContext } from '../Contexts';
-
-export function useMultisigAddressContext() {
-	return useContext(MultisigAddressContext);
-}
